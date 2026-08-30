9212d97e52dea8a4e8e253f45ca9e1a9cece5fa7
darioMle <morettidariofr@gmail.com>
2026-08-30 20:06:13 +0100

    Add CORS support to Flask application

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from chatbot import ask_huggingface
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/chat", methods=["POST"])
 def chat():
