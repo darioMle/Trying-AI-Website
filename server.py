152c209eeeff16e24168981934149b8f3f98ddd9
darioMle <morettidariofr@gmail.com>
2026-09-04 19:35:13 +0100

    Change chatbot function to ask_deepinfra

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from chatbot import ask_groq
+from chatbot import ask_deepinfra
 import os
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ CORS(app)
 @app.route("/chat", methods=["POST"])
 def chat():
     user_message = request.json["message"]
-    bot_reply = ask_groq(user_message)
+    bot_reply = ask_deepinfra(user_message)
     return jsonify({"reply": bot_reply})
 
 if __name__ == "__main__":
