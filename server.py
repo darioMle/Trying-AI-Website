a12f1dff82013920c1fbc97ebc38618d9cdc339b
darioMle <morettidariofr@gmail.com>
2026-09-06 09:13:13 +0100

    Change chatbot function to ask_deepinfra

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from chatbot import ask_together
+from chatbot import ask_deepinfra
 import os
 
 app = Flask(__name__)
@@ -9,10 +9,9 @@ CORS(app)
 @app.route("/chat", methods=["POST"])
 def chat():
     user_message = request.json["message"]
-    bot_reply = ask_together(user_message)
+    bot_reply = ask_deepinfra(user_message)
     return jsonify({"reply": bot_reply})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
